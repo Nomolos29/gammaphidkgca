897daca34715e798113d501913101d9f89b5f2ed
Sunday Solomon <nomolos2019@gmail.com>
2026-02-05 18:36:30 +0100

    Added a not found page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/layouts";
+import { Footer, Header } from "@/components/layouts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +30,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
